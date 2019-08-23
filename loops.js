99b31b7ccc4323a53284f724940f8a29d21f641d
Stephen Robert hall <steve.r.hall@outlook.com>
2019-08-23 08:31:36 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,27 @@
+function forLoop(array) {
+  var lit1;
+  for (let i=0; i<25; i++) {
+    if (i === 0) {lit1 = "I am 1 strange loop."}
+    if (i === 13) {lit1 = "I am 1 strange loop."}
+    else {lit1 = "I am " + i + " strange loops." } ;
+    array.unshift(lit1) ;
+  }
+  return array
+}
+
+function whileLoop(countdown) {
+    while (countdown > 0) {
+      console.log(--countdown);
+        }
+  return `done`
+}
+
+function doWhileLoop(int) {
+   var cnt = 0;
+    do {
+      console.log("I run once regardless.");
+      cnt = cnt + 1;
+    }
+    while (cnt < int);
+  return `done`
+}
