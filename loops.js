9f6f9d83797e2d95c4a05ca3d4044b0445a87822
Stephen Robert hall <steve.r.hall@outlook.com>
2019-08-23 08:51:49 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 function forLoop(array) {
   var lit1;
   for (let i=0; i<25; i++) {
-    if (i === 0) {lit1 = "I am 1 strange loop."}
+    if (i === 1) {lit1 = "I am 1 strange loop."}
     else {lit1 = "I am " + i + " strange loops." } ;
     array.push(lit1) ;
   }
